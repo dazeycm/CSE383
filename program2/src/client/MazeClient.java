bff08123d80534e36e296df57f2b547b4444dd29
dazeycm <craigdazey@gmail.com>
2015-09-22 22:11:24 -0400

    should probably use git more

---
+++ program2/src/client/MazeClient.java
@@ -0,0 +1,82 @@
+package client;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class MazeClient {
+	XmlRpcClient xmlRPCclient = null;
+	int port = -1;
+	String id = "";
+	
+	public MazeClient(String host,int p) throws IOException {
+		port = p;
+		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+		config.setServerURL(new URL("http://" + host+":"+port));
+		xmlRPCclient = new XmlRpcClient();
+		xmlRPCclient.setConfig(config);
+	}
+	
+	public void run() {
+		try{
+			connect();
+			
+			while(true) {
+				
+			}
+		} 
+		catch(Exception err) {
+			System.err.println("Error in main run loop: " + err);
+			return;
+		}
+	}
+	
+	public void connect() throws XmlRpcException  {
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.print("Username: ");
+		String username = kb.next();
+		
+		System.out.println("Password: ");
+		String password = kb.next();
+		
+		id = connectToServer(username, password);
+		if (id.equals("-1")) {
+			throw new XmlRpcException("Username is already in use");
+		}
+	}
+	
+	public String connectToServer(String user, String pass) throws XmlRpcException {
+		Object[] params = new Object[]{user, pass};
+		String result= (String) xmlRPCclient.execute("mazehandler.connect", params);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err.println("Usage:  java AddMessage <HOST> <PORT>");
+		}
+		
+		int port = -1;
+		try {
+			port = Integer.parseInt(args[1]);
+		} catch (Exception err) {
+			System.out.println("specify port");
+			return;
+		}
+
+		try {
+			MazeClient client = new MazeClient(args[0],port);
+			client.run();
+		}
+		catch (Exception err) {
+			System.err.println("Error initializing client: " + err);
+			return;
+		}
+	}
+	
+}
---
+++ program2/src/server/MazeHandler.java
@@ -0,0 +1,73 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MazeHandler {
+
+	public final static int MAZESIZE = 9;
+	
+	public static char[][] maze = new char[MAZESIZE][MAZESIZE];
+	public static boolean mazeInit = false;
+	public static Map<Integer, User> sid = new HashMap<Integer, User>();
+	public static int currId = -1;
+	
+	public String connect(String user, String pass) {
+		for(Map.Entry<Integer, User> entry : sid.entrySet()) {
+			if (entry.getValue().getUsername().equals(user)) {
+				return "-1"; // return -1 to represent that username already exists
+			}
+		}
+		currId++;
+		sid.put(currId, new User(user, pass));
+		return Integer.toString(currId);
+	}
+	
+	public String close(String sid, String pass) {
+		//need to set in db to done
+		
+		if(!this.sid.containsKey(sid)) {
+			return "SID not found";
+		}
+		
+		if(!this.sid.get(sid).getPassword().equals(pass)) {
+			return "Password not found";
+		}
+		
+		return "OK";
+	}
+	
+	public void createMazeArray() {
+		
+		if(mazeInit)
+			return;	//check to see if array has already been initialized
+			
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("maze.in"));
+			int row = 0;
+			int column = 0;
+			
+			while (in.ready()) { 
+				  String text = in.readLine();
+				  for(char c : text.toCharArray()) {
+					  maze[row][column] = c;
+					  if(column + 1 > MAZESIZE - 1)
+						  column = 0;
+					  else
+						  column++;
+				  }
+				  row++;
+				}
+		} catch (FileNotFoundException e) {
+			System.err.println("File with maze could not be found");
+		} catch (IOException e) {
+			System.err.println("Could not read from file");
+		}
+		
+		mazeInit = true;
+	}
+}
